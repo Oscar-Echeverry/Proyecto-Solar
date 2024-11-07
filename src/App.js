672285e0bf5580a08cd57f4f5d0622f6669258fa
Oscar Echeverry <echeverryoscar123@gmail.com>
2024-11-07 13:18:33 -0500

    proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';  // Importa el componente Home
+import Instalacion from './pages/Instalacion';
+import Educacion from './pages/Educacion';
+import Ciberseguridad from './pages/Ciberseguridad';
+import Monitoreo from './pages/Monitoreo';
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <div className="content">
+        <Routes>
+          {/* La Home debe ser la página principal */}
+          <Route path="/" element={<Home />} />
+          <Route path="/instalacion" element={<Instalacion />} />
+          <Route path="/educacion" element={<Educacion />} />
+          <Route path="/ciberseguridad" element={<Ciberseguridad />} />
+          <Route path="/monitoreo" element={<Monitoreo />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
